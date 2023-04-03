424ed3c0e8c6157383e63e10b85825e69757fb23
mradovic10 <radovic.miodrag10@gmail.com>
2023-04-03 14:29:16 -0400

    Charting of earthquake markers on the map with their respective magnitude size and depth color.

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -2,30 +2,57 @@
 const url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
 
 var map = L.map('map', {
-    center: [39.49, -98.34],
-    zoom: [4]
+    center: [29.9579005446342, 9.559838893763516],
+    zoom: [2]
 });
 
 // Add a tile layer.
-L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(map);
+var OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+
+OpenStreetMap_Mapnik.addTo(map);
 
 // Perform a GET request to the query URL.
 d3.json(url).then(function (data) {
 
     let earthquakes = data.features;
 
-    console.log(earthquakes[0].properties.mag);
+    //console.log(earthquakes[0].properties.mag);
 
-    //for (let i = 0; i < earthquakes.length; i++) {
+    //console.log(earthquakes[0].geometry.coordinates[2])
 
-    L.circle([earthquakes[0].geometry.coordinates[1], earthquakes[0].geometry.coordinates[0]], {
-        color: 'yellow',
-        fillColor: 'yellow',
-        fillOpacity: 0.5,
-        radius: earthquakes[0].properties.mag * 50000
-    }).addTo(map);
+    console.log(earthquakes.length)
+
+    function color(depth) {
+        
+        if (depth >= -10 && depth < 10) {
+            return 'chartreuse';
+        } else if (depth >= 10 && depth < 30) {
+            return 'greenyellow';
+        } else if (depth >= 30 && depth < 50) {
+            return 'sandybrown';
+        } else if (depth >= 50 && depth < 70) {
+            return 'orange';
+        } else if (depth >= 70 && depth < 90) {
+            return 'orangered';
+        } else if (depth >= 90) {
+            return 'red';
+        };
+
+    };
+        
+    for (let i = 0; i < earthquakes.length; i++) {
+
+        L.circle([earthquakes[i].geometry.coordinates[1], earthquakes[i].geometry.coordinates[0]], {
+            color: 'black',
+            weight: 0.5,
+            fillColor: color(earthquakes[i].geometry.coordinates[2]),
+            fillOpacity: 0.5,
+            radius: earthquakes[i].properties.mag * 50000
+        }).addTo(map);
+
+    };
 
 });
